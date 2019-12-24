f90dcd45cabe2da3b56363bb697a6d580392b3c5
Matheus <dr_4_gao@hotmail.com>
2019-12-24 10:17:12 -0300

    Começando com o formulário

--- screens/NewPlaceScreen.js
+++ screens/NewPlaceScreen.js
@@ -1,14 +1,57 @@
-import React from 'react';
-import { View, Text, StyleSheet } from 'react-native';
+import React, { useEffect, useState } from "react";
+import {
+  View,
+  ScrollView,
+  Text,
+  TextInput,
+  StyleSheet,
+  Button
+} from "react-native";
+import Colors from "../constants/Colors";
 
 const NewPlaceScreen = props => {
+  const [title, setTitle] = useState();
+
+  const titleHandlerChange = text => {
+    setTitle(text);
+  };
+
+  const savePlaceHandler = () => {};
+
   return (
-    <View>
-      <Text>NewPlaceScreen</Text>
-    </View>
+    <ScrollView>
+      <View style={styles.form}>
+        <Text style={styles.label}>Título</Text>
+        <TextInput
+          style={styles.textInput}
+          onChangeText={titleHandlerChange}
+          value={title}
+        />
+        <Button
+          title="Salvar local"
+          color={Colors.primary}
+          onPress={savePlaceHandler}
+        />
+      </View>
+    </ScrollView>
   );
 };
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  form: {
+    margin: 30
+  },
+  label: {
+    fontSize: 18,
+    marginBottom: 15
+  },
+  textInput: {
+    borderBottomColor: "#ccc",
+    borderBottomWidth: 1,
+    marginBottom: 15,
+    paddingVertical: 4,
+    paddingHorizontal: 2
+  }
+});
 
 export default NewPlaceScreen;
