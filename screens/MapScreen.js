9254a37233a3aa63a9723fb31a6f2c0e6927a90d
Matheus <dr_4_gao@hotmail.com>
2019-12-25 12:55:18 -0300

    Colocando marcação no mapa

--- screens/MapScreen.js
+++ screens/MapScreen.js
@@ -1,15 +1,40 @@
-import React from "react";
-import { View, Text, StyleSheet } from "react-native";
-import MapView from "react-native-maps";
+import React, { useState } from "react";
+import { StyleSheet } from "react-native";
+import MapView, { Marker } from "react-native-maps";
 
 const MapScreen = props => {
+  const [location, setLocation] = useState();
+
   const mapRegion = {
     latitude: 37.2,
     longitude: 35.3,
     latitudeDelta: 0.0922,
     longitudeDelta: 0.0421
   };
-  return <MapView style={styles.map} region={mapRegion} />;
+
+  const selectHandler = event => {
+    setLocation({
+      lat: event.nativeEvent.coordinate.latitude,
+      lng: event.nativeEvent.coordinate.longitude
+    });
+  };
+
+  let markerCoordinates;
+
+  if (location) {
+    markerCoordinates = {
+      latitude: location.lat,
+      longitude: location.lng
+    };
+  }
+
+  return (
+    <MapView style={styles.map} region={mapRegion} onPress={selectHandler}>
+      {markerCoordinates && (
+        <Marker title="Lugar escolhido" coordinate={markerCoordinates} />
+      )}
+    </MapView>
+  );
 };
 
 const styles = StyleSheet.create({
